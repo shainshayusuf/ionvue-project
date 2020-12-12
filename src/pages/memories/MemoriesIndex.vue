27dc84608a7be4ee56b0f8feeaf7d9aa43cf2b70
shainshay <shainshay@naanal.in>
2020-12-12 12:10:20 +0530

    first commit

---
+++ src/pages/memories/MemoriesIndex.vue
@@ -0,0 +1,30 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>
+                    All Memories
+                </ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <ion-list>
+<ion-item>Max</ion-item>
+<ion-item>Max</ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import {IonHeader,IonTitle,IonToolbar,IonContent,IonPage}  from '@ionic/vue'
+export default {
+    components:{
+      IonHeader,IonTitle,IonToolbar,IonContent,IonPage  
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
